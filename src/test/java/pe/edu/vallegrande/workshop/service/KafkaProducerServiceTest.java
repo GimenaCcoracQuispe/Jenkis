d54377c1af9ca2dad070f6e3ae7d43680493e91f
Gimena Ccoracc Quispe <gimena.ccorac@vallegrande.edu.pe>
2025-05-29 17:37:25 +0000

    cambios7

--- src/test/java/pe/edu/vallegrande/workshop/service/KafkaProducerServiceTest.java
+++ src/test/java/pe/edu/vallegrande/workshop/service/KafkaProducerServiceTest.java
@@ -20,7 +20,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import pe.edu.vallegrande.workshop.dto.WorkshopKafkaEventDto;
 
 @ExtendWith(MockitoExtension.class)
-class KafkaProducerServiceTest {
+public class KafkaProducerServiceTest {
 
     @Mock
     private KafkaTemplate<String, String> kafkaTemplate;
