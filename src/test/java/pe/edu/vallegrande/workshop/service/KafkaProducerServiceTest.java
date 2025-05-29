6353a4afb2713eedaa0bab6cae5474c3d802278f
Gimena Ccoracc Quispe <gimena.ccorac@vallegrande.edu.pe>
2025-05-29 17:11:54 +0000

    cambios3

--- src/test/java/pe/edu/vallegrande/workshop/service/KafkaProducerServiceTest.java
+++ src/test/java/pe/edu/vallegrande/workshop/service/KafkaProducerServiceTest.java
@@ -4,12 +4,16 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.util.concurrent.CompletableFuture;
+
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -17,6 +21,7 @@ import pe.edu.vallegrande.workshop.dto.WorkshopKafkaEventDto;
 
 @ExtendWith(MockitoExtension.class)
 public class KafkaProducerServiceTest {
+
     @Mock
     private KafkaTemplate<String, String> kafkaTemplate;
 
@@ -33,8 +38,12 @@ public class KafkaProducerServiceTest {
         String mensaje = "{\"id\":1}";
 
         when(objectMapper.writeValueAsString(eventoDto)).thenReturn(mensaje);
-        when(kafkaTemplate.send(anyString(), anyString(), anyString()))
-            .thenReturn(null); // Puedes mockear CompletableFuture si quieres
+
+        ProducerRecord<String, String> record = new ProducerRecord<>("workshop-events", "1", mensaje);
+        CompletableFuture<SendResult<String, String>> future = CompletableFuture
+                .completedFuture(new SendResult<>(record, null));
+
+        when(kafkaTemplate.send(anyString(), anyString(), anyString())).thenReturn(future);
 
         kafkaProducerService.sendWorkshopEvent(eventoDto);
 
@@ -42,18 +51,18 @@ public class KafkaProducerServiceTest {
         verify(kafkaTemplate).send("workshop-events", "1", mensaje);
     }
 
-
-   @Test
+    @Test
     void testSendWorkshopEvent_exception() throws Exception {
         WorkshopKafkaEventDto eventoDto = new WorkshopKafkaEventDto();
         eventoDto.setId(1L);
 
         when(objectMapper.writeValueAsString(eventoDto))
-            .thenThrow(new RuntimeException("Falla de serialización"));
+                .thenThrow(new RuntimeException("Falla de serialización"));
 
         kafkaProducerService.sendWorkshopEvent(eventoDto);
 
         verify(objectMapper).writeValueAsString(eventoDto);
-        // El catch se cubrirá porque la excepción es atrapada internamente
+        // no verify en kafkaTemplate porque no se llega a llamar por la excepción
     }
+
 }
